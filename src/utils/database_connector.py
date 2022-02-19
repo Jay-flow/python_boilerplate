f0e2a9b1565d33bcb29edba448e0073d14c85293
Jay-flow <kimpetro153@gmail.com>
2022-02-19 11:09:18 +0900

    Create a abstracts directory

--- src/utils/database_connector.py
+++ src/utils/database_connector.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from src.models.singleton import Singleton
+from src.abstracts.singleton import Singleton
 from src.utils.env import get
 
 
