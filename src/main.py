dbdc044f045189aa5ea3c2a6a21a4f8f96fc8635
Jay-flow <kimpetro153@gmail.com>
2022-02-19 12:40:04 +0900

    Set the logger

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from src.utils.logger import logger
+
+
 class Main:
     def run(self) -> None:
-        print("Run!")
+        logger.decorate_log("Run!")
 
 
 if __name__ == '__main__':
---
+++ src/utils/logger_colored_formatter.py
@@ -0,0 +1,43 @@
+import logging
+
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+# The background is set with 40 plus the number of the color, and the foreground with 30
+
+# These are the sequences need to get colored output
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+
+def formatter_message(message: str, use_color: bool = True) -> str:
+    if use_color:
+        message = message.replace(
+            "$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+
+class LoggerColoredFormatter(logging.Formatter):
+    def __init__(self, msg: str, use_color: bool = True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record: logging.LogRecord):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (
+                30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
